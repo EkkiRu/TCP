9d3e44b71895ec3fe444dd02548618ba101d6128
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2020-11-20 14:38:24 +0600

    Homework
    Homework

---
+++ TCPClient/TCPClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var random = new Random();
+            var name = random.Next(500, 2500).ToString();
+            while (true)
+            {
+                Console.WriteLine("Введите сообщение:");
+
+                var text = $"name:{name} - {Console.ReadLine()}";
+                //записываем ip аддресс и порт
+                var ipAddress = IPAddress.Parse("127.0.0.1");
+                var port = 62227;
+
+                //создаем TCP Listener
+                var client = new TcpClient();
+
+                try
+                {
+                    await client.ConnectAsync(ipAddress, port);
+
+                    var stream = client.GetStream();
+                    var data = Encoding.UTF8.GetBytes(text);
+                    await stream.WriteAsync(data, 0, data.Length);
+
+                    stream.Close();
+                    client.Close();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+    }
+}
---
+++ TCPServer/TCPServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //записываем ip аддресс и порт
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+            var port = 62227;
+
+            //создаем TCP Listener
+            var listemer = new TcpListener(ipAddress, port);
+
+            try
+            {
+                //Начинаем слушать входящие соединения
+                listemer.Start();
+
+                while (true)
+                {
+                    var incomingConnection = await listemer.AcceptTcpClientAsync();
+                  _ = Task.Run(() =>
+                    {
+                        var networkStream = incomingConnection.GetStream();
+                        var stringBuilder = new StringBuilder();
+                        do
+                        {
+                            var buffer = new byte[512];
+                            networkStream.Read(buffer, 0, buffer.Length);
+                            stringBuilder.Append(Encoding.UTF8.GetString(buffer));
+                        } while (networkStream.DataAvailable);
+
+                        Console.WriteLine($"Сообщение - {stringBuilder.ToString()}");
+
+                        networkStream.Close();
+                        incomingConnection.Close();
+                    });
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
